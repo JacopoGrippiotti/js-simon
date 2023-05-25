e54f5573e638e1103b6d3fe45994f53638b87137
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-05-25 22:39:57 +0200

    fixed script.js file to update time displayed every second

--- script.js
+++ script.js
@@ -1,29 +1,64 @@
+
 function missingTimeInMilliseconds(endDate){
 
     let missingTime = Date.parse(endDate) - Date.parse(new Date())
+    let missingSeconds = Math.floor((missingTime / 1000) % 60)
+
+    let missingMinutes = Math.floor((missingTime / 1000 / 60) % 60)
+
+    let missingHours = Math.floor((missingTime / (1000*60*60)) % 24)
+
+    let missingDays = Math.floor(missingTime / (1000 * 60 * 60 * 24))
+   
+    return [missingDays, missingHours, missingMinutes, missingSeconds]
+}
+
+setInterval(missingTimeInMilliseconds, 1000, '2023-05-26T09:30:00.000+02:00')
+
+let secondsSpan = document.querySelector('span.seconds')
     
-   return missingTime
+let minutesSpan = document.querySelector('span.minutes')
+    
+let hoursSpan = document.querySelector('span.hours')
+    
+let daysSpan = document.querySelector('span.days')
+
+let span = document.querySelectorAll('span')
+
+function displayMissingTime(){
+
+     let missingTime = missingTimeInMilliseconds('2023-05-26T09:30:00.000+02:00')
+
+     let missingDays = missingTime[0]
+
+     let missingHours = missingTime[1]
+
+     let missingMinutes = missingTime[2]
+
+     let missingSeconds = missingTime[3]
+        
+     secondsSpan.innerHTML = ''
+     secondsSpan.innerHTML += missingSeconds
+
+     minutesSpan.innerHTML = ''
+     minutesSpan.innerHTML += missingMinutes
+
+     hoursSpan.innerHTML = ''
+     hoursSpan.innerHTML += missingHours
+
+     daysSpan.innerHTML = ''
+     daysSpan.innerHTML += missingDays  
 }
 
-let missingTime = missingTimeInMilliseconds('2023-05-26T09:30:00.000+02:00')
-
-let missingSeconds = Math.floor((missingTime / 1000) % 60)
-
-let missingMinutes = Math.floor((missingTime / 1000 / 60) % 60)
-
-let missingHours = Math.floor((missingTime / (1000*60*60)) % 24)
-
-let missingDays = Math.floor(missingTime / (1000 * 60 * 60 * 24))
-
-function displayMissingTime(days, hours, minutes, seconds){
-
-
-}
+ 
 
 setInterval(displayMissingTime, 1000)
+
 function lessonStarted(){
 
     alert('FORZA ADESSO INIZIA LA LEZIONE')
 }
+
+
 
 
