24c807101ea5901fbe4bca3ac4329c58d60006fb
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-05-25 21:21:10 +0200

    fixed script.js to take variables values out of missingTime function

--- script.js
+++ script.js
@@ -1,33 +1,26 @@
-function missingTimeVariables(endDate){
+function missingTimeInMilliseconds(endDate){
 
     let missingTime = Date.parse(endDate) - Date.parse(new Date())
-
-    let missingSeconds = Math.floor((missingTime / 1000) % 60)
-
-    let missingMinutes = Math.floor((missingTime / 1000 / 60) % 60)
-
-    let missingHours = Math.floor((missingTime / (1000*60*60)) % 24)
-
-    let missingDays = Math.floor(missingTime / (1000 * 60 * 60 * 24))
-
-   return [missingTime,missingSeconds,missingMinutes,missingHours,missingDays ]
+    
+   return missingTime
 }
 
-const timeVariables = missingTimeVariables('2023-05-26T09:30:00.000+02:00')
+let missingTime = missingTimeInMilliseconds('2023-05-26T09:30:00.000+02:00')
 
-const totalTimeMissing = timeVariables[0]
+let missingSeconds = Math.floor((missingTime / 1000) % 60)
 
-const secondsMissing = timeVariables[1]
+let missingMinutes = Math.floor((missingTime / 1000 / 60) % 60)
 
-const minutesMissing = timeVariables[2]
+let missingHours = Math.floor((missingTime / (1000*60*60)) % 24)
 
-const hoursMissing = timeVariables[3]
+let missingDays = Math.floor(missingTime / (1000 * 60 * 60 * 24))
 
-const daysMissing = timeVariables[4]
-
-
+function displayMissingTime(days, hours, minutes, seconds){
 
 
+}
+
+setInterval(displayMissingTime, 1000)
 function lessonStarted(){
 
     alert('FORZA ADESSO INIZIA LA LEZIONE')
