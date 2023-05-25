807325f03fcdd410111cb7045fac480080789aa5
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-05-25 19:40:04 +0200

    added function missingTimeFromNow to script.js

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 function missingTimeFromNow(endDate){
 
-    let totalTime = Date.parse(endDate)
+    let missingTime = Date.parse(endDate) - Date.parse(new Date())
+
+    let missingSeconds = Math.floor((missingTime / 1000) % 60)
+
+    let missingMinutes = Math.floor((missingTime / 1000 / 60) % 60)
+
+    let missingHours = Math.floor((missingTime / (1000*60*60)) % 24)
+
+    let missingDays = Math.floor(missingTime / (1000 * 60 * 60 * 24))
+
+   return {
+
+    'missing time': missingTime,
+    
+    'missing seconds': missingSeconds,
+    
+    'missing minutes': missingMinutes,
+
+    'missing hours': missingHours,
+
+    'missing days': missingDays
+   }
 }
+
+const missingTime = missingTimeFromNow('2023-05-26T09:30:00.000+01:00')
+
+console.log(missingTime)
